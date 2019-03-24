9dae0dcf333cbbffd8027cd051f58fb4666931dd
Sasith Priyankara <priyankaramw@gmail.com>
2019-03-24 10:13:00 +0800

    2

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -1,8 +1,11 @@
 package com.londonappbrewery.iampoor;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startAnActivity (View view) {
+
+        EditText editName = (EditText)findViewById(R.id.editName);
+        String name = editName.getText().toString();
+
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra("name", name);
+        startActivity(intent);
     }
 }
